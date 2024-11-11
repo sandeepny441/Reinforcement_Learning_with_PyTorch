3456f3a7ae63eed8dceb4eb4654c234c005fe7c6
sandeepny441 <sandeepny441@gmail.com>
2024-11-10 23:58:15 -0600

    added spotify Rec model

---
+++ a_pyTorch/spotifyRec_BART/a.py
@@ -0,0 +1,8 @@
+state = {
+    'user_history': recent_listens,
+    'time_context': time_of_day,
+    'device_type': current_device,
+    'session_depth': songs_in_session,
+    'previous_skips': skip_pattern,
+    'genre_distribution': genre_vector
+}
