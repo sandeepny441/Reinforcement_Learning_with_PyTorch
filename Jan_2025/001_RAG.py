5740b6404dc94119097821be8c67c123834ab680
sandeepny441 <sandeepny441@gmail.com>
2025-01-01 12:06:49 -0600

    RAG 001

---
+++ Jan_2025/001_RAG.py
@@ -0,0 +1,8 @@
+Introduction
+Over the last couple of weeks, we have spent plenty of time understanding the key components of real-world NLP systems:
+
+In addition to this, we have also talked about vector databases, which are not new, but they have become heavily prevalent lately in the GenAI era, primarily due to their practical utility not just in LLMs but in other applications as well.
+
+More specifically, vector databases come in handy in building RAG applications, which is a technique that combines the strengths of large language models (LLMs) with external knowledge sources.
+
+This is depicted below.
